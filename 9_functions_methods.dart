6fdaa5e631ca1fdf447467a166c6143f7fbe0896
Jiho Rhee <rhee519@gmail.com>
2023-07-16 09:32:38 +0900

    9. Named & Default parameters

--- 9_functions_methods.dart
+++ 9_functions_methods.dart
@@ -2,6 +2,11 @@ void main(List<String> args) {
   findPerimeter(4, 2);
   int rectArea = getArea(10, 5);
   print("The area is $rectArea");
+
+  printCities("Seoul", "Tokyo"); // "Seoul", "Tokyo", null
+
+  int volume = findVolume(10, width: 5, height: 3);
+  print("Volume: $volume");
 }
 
 /**
@@ -30,3 +35,23 @@ int getArea(int len, int width) {
  * 화살표 함수로도 정의할 수 있다.
  */
 int getAreaArrow(int len, int width) => len * width;
+
+/**
+ * Required & Optional parameters
+ */
+void printCities(String name1, String name2, [String? name3]) {
+  print("Name1: $name1");
+  print("Name2: $name2");
+  print("Name3: $name3");
+}
+
+/**
+ * Named & Default parameter
+ * {} 로 묶은 param들은 nullability를 명시해야 한다.
+ * - required를 붙이거나
+ * - default 값을 설정하거나
+ * - optional함을 명시하거나
+ */
+int findVolume(int len, {required int width, int height = 10, bool? option}) {
+  return len * width * height;
+}
